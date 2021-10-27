c2630261f49e04c6b71987e9b594967727340606
Zuriel Díaz Agustín <zuriel_diaz_agustin@hotmail.com>
2021-10-27 01:24:19 -0500

    Interfaces Añadidas

--- app/src/main/java/com/app/apprecetario/MainActivity.kt
+++ app/src/main/java/com/app/apprecetario/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.app.apprecetario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imgBtn = findViewById<ImageButton>(R.id.imgBtnSopas)
+        imgBtn.setOnClickListener {
+            val ventanaSopas = Intent(this,CatalogoSopas::class.java)
+            startActivity(ventanaSopas)
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/app/apprecetario/NuevaRecetaSopa.kt
@@ -0,0 +1,14 @@
+package com.app.apprecetario
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class NuevaRecetaSopa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nueva_receta_sopa)
+
+
+    }
+}
