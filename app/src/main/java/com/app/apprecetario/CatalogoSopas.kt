37b19a4869e7a6ff03dd495d3a1ec0e556819657
Zuriel Díaz Agustín <zuriel_diaz_agustin@hotmail.com>
2021-10-27 20:46:26 -0500

    Actualizacion de las pantallas

--- app/src/main/java/com/app/apprecetario/CatalogoSopas.kt
+++ app/src/main/java/com/app/apprecetario/CatalogoSopas.kt
@@ -1,11 +1,21 @@
 package com.app.apprecetario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class CatalogoSopas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalogo_sopas)
+
+        val btnNuevaReceta = findViewById<Button>(R.id.btnNuevaReceta)
+        btnNuevaReceta.setOnClickListener {
+            val registroSopas = Intent(this,NuevaRecetaSopa::class.java)
+            startActivity(registroSopas)
+        }
+
     }
 }
